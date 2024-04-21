e2910b79fa5b98cf712f8066fbb06a9e0d0617d0
BasmaMounir <basmamounir57@gimal.com>
2024-04-22 00:58:18 +0200

    bmi calculator is finished

--- lib/BmiWidgets/Home/gender_widget.dart
+++ lib/BmiWidgets/Home/gender_widget.dart
@@ -1,25 +1,32 @@
+import 'package:bmi_calculator_app/BmiWidgets/Provider/BmiProvider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class GenderWidget extends StatefulWidget {
   String gender;
   String genderIcon;
+  bool isChecked;
 
-  GenderWidget({super.key, required this.gender, required this.genderIcon});
+  GenderWidget(
+      {super.key,
+      required this.gender,
+      required this.genderIcon,
+      required this.isChecked});
 
   @override
   State<GenderWidget> createState() => _GenderWidgetState();
 }
 
 class _GenderWidgetState extends State<GenderWidget> {
-  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
+    var provider = Provider.of<BmiProvider>(context);
     return Expanded(
       child: InkWell(
         onTap: () {
-          isChecked = !isChecked;
-          print('============ $isChecked');
+          widget.isChecked = !widget.isChecked;
+          provider.gender = widget.gender;
           setState(() {});
         },
         child: Container(
@@ -27,25 +34,25 @@ class _GenderWidgetState extends State<GenderWidget> {
           margin: const EdgeInsets.all(5),
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(12),
-              color: isChecked == true
-                  ? const Color(0xffdfe8f7)
+              color: widget.isChecked == true
+                  ? const Color(0xffffffff)
                   : const Color(0xffd8d8d8),
               //color: const Color(0xff2765cd),
               border: Border.all(
                   width: 2,
-                  color: isChecked == true
-                      ? const Color(0xff2765cd)
+                  color: widget.isChecked == true
+                      ? const Color(0xffDB4444)
                       : Colors.transparent)),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Visibility(
-                visible: isChecked == true ? true : false,
+                visible: widget.isChecked == true ? true : false,
                 child: const Align(
                   alignment: Alignment.topRight,
                   child: Icon(
-                    color: const Color(0xff2765cd),
+                    color: const Color(0xffDB4444),
                     Icons.check_circle,
                   ),
                 ),
@@ -53,8 +60,8 @@ class _GenderWidgetState extends State<GenderWidget> {
               ImageIcon(
                 AssetImage('${widget.genderIcon}'),
                 size: 80,
-                color: isChecked == true
-                    ? const Color(0xff2765cd)
+                color: widget.isChecked == true
+                    ? const Color(0xffDB4444)
                     : const Color(0xff888888),
               ),
               const SizedBox(
@@ -62,8 +69,8 @@ class _GenderWidgetState extends State<GenderWidget> {
               ),
               Center(
                   child: Text(
-                '${widget.gender}',
-                style: TextStyle(fontSize: 22),
+                    '${widget.gender}',
+                style: const TextStyle(fontSize: 22),
               ))
             ],
           ),
